7201bbfa3b29c67ab1969e3f4977a54b1e99849f
SapMagic01 <szekelyatti04@gmail.com>
2026-01-13 09:50:08 +0100

    02 fundamentals continue, break, looping

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -359,12 +359,33 @@ for (let i = 0; i <= jonas.length; i++) {
     types[i] = typeof jonas[i];
 }
 
+console.log(types);
 
+const years = [1991, 2007, 1969, 2020];
+const ages = [];
 
+for (let i = 0; i < years.length; i++) {
+    ages.push(2037 - years[i]);
+}
+console.log(ages);
 
+//continue and break
 
+console.log('Only strings');
+for (let i = 0; i < jonas.length; i++) {
+    if(typeof jonas[i] !== 'string') {
+        continue;
+    }
+    console.log(jonas[i], typeof jonas[i]);
+}
 
-
+console.log('Break with number');
+for (let i = 0; i < jonas.length; i++) {
+    if(typeof jonas[i] === 'number') {
+        break;
+    }
+    console.log(jonas[i], typeof jonas[i]);
+}
 
 
 
