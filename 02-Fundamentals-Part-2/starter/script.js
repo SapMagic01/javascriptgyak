77b94ac3d4fc9854c1cceb2ea0a9d5aba29756f6
SapMagic01 <szekelyatti04@gmail.com>
2026-01-13 10:27:40 +0100

    02 fundamentals while

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -387,7 +387,7 @@ for (let i = 0; i < jonas.length; i++) {
     console.log(jonas[i], typeof jonas[i]);
 }
 */
-
+/*
 const jonas = [
     'Jonas',
     'Schmedtmann',
@@ -410,11 +410,21 @@ for (let exercise = 1; exercise <= 4; exercise++) {
         console.log(`Lifting weight repetition ${rep}`);
     }
 }
+*/
 
+let rep = 1;
+while(rep <= 10) {
+    console.log(`Lifting weights repetition ${rep}`);
+    rep++;
+}
 
+let dice = Math.trunc(Math.random() * 6) + 1;
+console.log(dice);
 
-
-
+while (dice !== 6) {
+    console.log(`You rolled a ${dice}`);
+    if (dice === 6) console.log(`Loop is about to end`);
+}
 
 
 
