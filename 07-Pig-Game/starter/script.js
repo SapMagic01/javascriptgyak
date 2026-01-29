acc939fd0c1da73c8dad95f912ac35bf54cf911c
SapMagic01 <szekelyatti04@gmail.com>
2026-01-29 19:30:34 +0100

    holding, game end

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -16,12 +16,23 @@ const btnHold = document.querySelector('.btn--hold');
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+
 const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
+let playing = true;
+
+const switchPlayer = function() {
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    currentScore = 0;
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
+};
 
 //Rolling the dice func
 btnRoll.addEventListener('click', function(){
+    if (playing) {
     //1. generate random dice roll
 
     const dice = Math.trunc(Math.random() * 6) + 1
@@ -36,10 +47,29 @@ btnRoll.addEventListener('click', function(){
         document.getElementById(`current--${activePlayer}`).textContent = currentScore;
     } else{
         //switch to next player
-        document.getElementById(`current--${activePlayer}`).textContent = 0;
-        activePlayer = activePlayer === 0 ? 1 : 0;
-        currentScore = 0;
-        player0El.classList.toggle('player--active');
-        player1El.classList.toggle('player--active');
+        switchPlayer();
+        }
     }
-})
+});
+
+btnHold.addEventListener('click', function(){
+    if (playing) {
+    //1 add current score
+    scores[activePlayer] += currentScore;
+
+    document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
+
+    //2 check if players score > 100
+    if (scores[activePlayer] >= 10){
+        playing = false;
+        diceEl.classList.add('hidden');
+        document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
+        document.querySelector(`.player--${activePlayer}`).classList.remove('player--active');
+
+    } else{
+        //switch to next player
+        switchPlayer();
+        }
+    }
+
+});
