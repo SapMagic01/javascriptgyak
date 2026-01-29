89ccc6dec359ceecd695e3eda549fcdb213185f9
SapMagic01 <szekelyatti04@gmail.com>
2026-01-29 20:18:30 +0100

    dry

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -12,16 +12,27 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
+let scores, currentScore, activePlayer, playing;
 
 //Start
-score0El.textContent = 0;
-score1El.textContent = 0;
-diceEl.classList.add('hidden');
+const init = function () {
+     scores = [0, 0];
+     currentScore = 0;
+     activePlayer = 0;
+     playing = true;
 
-let scores = [0, 0];
-let currentScore = 0;
-let activePlayer = 0;
-let playing = true;
+    score0El.textContent = 0;
+    score1El.textContent = 0;
+    current0El.textContent = 0;
+    current1El.textContent = 0;
+
+    diceEl.classList.add('hidden');
+    document.querySelector(`.player--0`).classList.remove('player--winner');
+    document.querySelector(`.player--1`).classList.remove('player--winner');
+    player0El.classList.add('player--active');
+    player1El.classList.remove('player--active');
+};
+init();
 
 const switchPlayer = function() {
     document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -61,7 +72,7 @@ btnHold.addEventListener('click', function(){
     document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
 
     //2 check if players score > 100
-    if (scores[activePlayer] >= 10){
+    if (scores[activePlayer] >= 100){
         playing = false;
         diceEl.classList.add('hidden');
         document.querySelector(`.player--${activePlayer}`).classList.add('player--winner');
@@ -75,21 +86,4 @@ btnHold.addEventListener('click', function(){
 
 });
 
-btnNew.addEventListener('click', function(){
-    //reset scores
-    //remove winner class
-    scores = [0, 0];
-    currentScore = 0;
-    activePlayer = 0;
-    score0El.textContent = 0;
-    score1El.textContent = 0;
-    current0El.textContent = 0;
-    current1El.textContent = 0;
-    playing = true;
-    document.querySelector(`.player--0`).classList.remove('player--winner');
-    document.querySelector(`.player--1`).classList.remove('player--winner');
-    player0El.classList.add('player--active');
-    player1El.classList.remove('player--active');
-    diceEl.classList.add('hidden');
-
-})
+btnNew.addEventListener('click', init)
