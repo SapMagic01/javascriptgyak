654a942c316d9cf1adc125923023dd38c30693eb
SapMagic01 <szekelyatti04@gmail.com>
2026-01-29 20:11:04 +0100

    reset gomb

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -12,12 +12,13 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
+
 //Start
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
-const scores = [0, 0];
+let scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
 let playing = true;
@@ -73,3 +74,22 @@ btnHold.addEventListener('click', function(){
     }
 
 });
+
+btnNew.addEventListener('click', function(){
+    //reset scores
+    //remove winner class
+    scores = [0, 0];
+    currentScore = 0;
+    activePlayer = 0;
+    score0El.textContent = 0;
+    score1El.textContent = 0;
+    current0El.textContent = 0;
+    current1El.textContent = 0;
+    playing = true;
+    document.querySelector(`.player--0`).classList.remove('player--winner');
+    document.querySelector(`.player--1`).classList.remove('player--winner');
+    player0El.classList.add('player--active');
+    player1El.classList.remove('player--active');
+    diceEl.classList.add('hidden');
+
+})
