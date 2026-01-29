b31b2348307d93653ac98e84643a52975b727045
SapMagic01 <szekelyatti04@gmail.com>
2026-01-29 18:50:31 +0100

    rolling dice 1 jatekos

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,1 +1,36 @@
 'use strict';
+
+//Selecting elements
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+//Start
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
+let currentScore = 0;
+
+//Rolling the dice func
+btnRoll.addEventListener('click', function(){
+    //1. generate random dice roll
+
+    const dice = Math.trunc(Math.random() * 6) + 1
+    console.log(dice);
+    //2. display dice
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
+
+    //3. check if rolled = 1: if true next player
+    if (dice !== 1){
+        currentScore += dice;
+        current0El.textContent = currentScore;
+    } else{
+
+    }
+})
