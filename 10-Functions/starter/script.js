5f2db867d059579446aee6cbcfd5c69b4d91480a
SapMagic01 <szekelyatti04@gmail.com>
2026-02-10 13:08:40 +0100

    passing arguments

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,1 +1,29 @@
 'use strict';
+
+const flight = 'LH234';
+const jonas = {
+    name: 'Jonas Schmedtmann',
+    passport: 213123312,
+}
+
+const checkIn = function(flightNum, passenger) {
+    flightNum = 'LH999';
+    passenger.name = 'Mr. ' + passenger.name;
+
+    if(passenger.passport === 213123312) {
+    alert('Check in')
+    } else {
+        alert('Wrong passport')
+    }
+}
+/*
+checkIn(flight, jonas);
+console.log(flight);
+console.log(jonas);
+*/
+
+const newPassport = function(person){
+    person.passport = Math.trunc(Math.random() * 100000);
+}
+newPassport(jonas);
+checkIn(flight, jonas);
