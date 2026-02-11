5b1ef0aba2f7270fb46cc5a3ddadcfd0b4966bfb
SapMagic01 <szekelyatti04@gmail.com>
2026-02-11 12:17:00 +0100

    functions accepting callback functions

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*
 const flight = 'LH234';
 const jonas = {
     name: 'Jonas Schmedtmann',
@@ -16,14 +17,43 @@ const checkIn = function(flightNum, passenger) {
         alert('Wrong passport')
     }
 }
+*/
 /*
 checkIn(flight, jonas);
 console.log(flight);
 console.log(jonas);
-*/
+
 
 const newPassport = function(person){
     person.passport = Math.trunc(Math.random() * 100000);
 }
 newPassport(jonas);
 checkIn(flight, jonas);
+*/
+
+const oneWord = function(str) {
+    return str.replace(/ /g, '').toLowerCase();
+}
+
+const upperFirstWord = function(str) {
+    const [first, ...others] = str.split(' ');
+    return [first.toUpperCase(), ...others].join(' ');
+}
+
+//Higher-order function
+const transformer = function(str, fn){
+    console.log(`Original string: ${str}`);
+    console.log(`Transformed string: ${fn(str)}`)
+    console.log(`Transformed by: ${fn.name}`);
+
+}
+transformer('JavaScript is the best', upperFirstWord);
+
+transformer('JavaScript is the best', oneWord);
+
+const high5 = function() {
+    console.log('hi🖐️');
+}
+document.body.addEventListener('click', high5);
+
+['jonas', 'martha', 'adam'].forEach(high5)
