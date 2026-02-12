5be5f624b2ff51932cf09be2695e2e3c04f8bf1b
SapMagic01 <szekelyatti04@gmail.com>
2026-02-12 14:28:55 +0100

    at method

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -74,7 +74,7 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
-
+/*
 let arr = ['a', 'b', 'c', 'd', 'e'];
 
 //Slice
@@ -105,4 +105,17 @@ console.log(letters);
 console.log([...arr, ...arr2]);
 
 //Join
-console.log(letters.join('-'));
+console.log(letters.join('-'));*/
+
+const arr = [23, 11, 64];
+console.log(arr[0]);
+console.log(arr.at(0));
+
+//Getting last array element
+console.log(arr[arr.length - 1]);
+console.log(arr.slice(-1)[0]);
+
+console.log(arr.at(-1));
+
+console.log('jonas'.at(0));
+console.log('jonas'.at(-1));
