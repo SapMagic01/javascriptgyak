a20111512095b590028ffad9a6780d4ba5d5f5d0
SapMagic01 <szekelyatti04@gmail.com>
2026-02-12 15:03:49 +0100

    challenge

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -137,3 +137,21 @@ console.log(arr.at(-1));
 console.log('jonas'.at(0));
 console.log('jonas'.at(-1));
 */
+
+//Julia test data [3, 5, 2, 12, 7] Kate test data [4, 1, 15, 8, 3]
+//Julie test data 2 [9, 16, 6, 8, 3] Kate test data 2 [10, 5, 6, 1, 4]
+
+function checkDogs (dogsJulia, dogsKate){
+  const dogsJuliaCorrected = dogsJulia.slice();
+  dogsJuliaCorrected.splice(0, 1);
+  dogsJuliaCorrected.splice(-2);
+  const dogs = dogsJuliaCorrected.concat(dogsKate);
+  dogs.forEach(function(dog, i){
+    if(dog >= 3){
+      console.log(`Dog number ${i + 1} is an adult and is ${dog} years old`);
+    } else{
+      console.log(`Dog number ${i + 1} is still a puppy`);
+    }
+  })
+}
+checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
